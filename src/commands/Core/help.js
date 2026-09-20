26a206b8c0dcd6b3c5e88b1bb0c5731fd0f3d02a
AstrxlzzL <mbdfha@gmail.com>
2026-09-20 14:02:18 -0600

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -100,7 +100,7 @@ export async function createInitialHelpMenu(client) {
             },
             {
                 name: '\u200B',
-                value: `-# ${botName} DC: https://discord.com/invite/vx73`,
+                value: `-# ${botName} DC: https://discord.gg/vx73`,
                 inline: false,
             },
         ],
