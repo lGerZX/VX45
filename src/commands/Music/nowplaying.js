09f3374d28401fa102fec327058360b7494fcce0
AstrxlzzL <mbdfha@gmail.com>
2026-09-20 18:10:24 -0600

    Update nowplaying.js

--- src/commands/Music/nowplaying.js
+++ src/commands/Music/nowplaying.js
@@ -7,7 +7,7 @@ export default {
     category: 'Music',
     data: new SlashCommandBuilder()
         .setName('nowplaying')
-        .setDescription('Show the currently playing track'),
+        .setDescription('Mostrar la pista que se esta reproduciendo actualmente'),
 
     async execute(interaction, config, client) {
         await deferMusicCommand(interaction);
