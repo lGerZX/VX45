89bc929f950717727ee7364b2808927b1be02a42
AstrxlzzL <mbdfha@gmail.com>
2026-09-24 17:25:28 -0600

    Update support.js

--- src/commands/Core/support.js
+++ src/commands/Core/support.js
@@ -3,7 +3,7 @@ import { createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const SUPPORT_SERVER_URL = "https://discord.gg/vx73";
+const SUPPORT_SERVER_URL = "https://discord.gg/vx45";
 
 export default {
     data: new SlashCommandBuilder()
